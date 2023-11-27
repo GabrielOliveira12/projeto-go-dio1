77cbc6bb6317640887f5ac5d1ce84eeb2480adf7
gabriel <gaabriel_profissional@hotmail.com>
2023-11-27 14:14:51 -0300

    commit

---
+++ projeto1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const ebulicaoK = 373.0
+
+func main() {
+
+tempK := ebulicaoK
+tempC := tempK - 273.0
+
+fmt.Printf("A temperatura de ebulição da água em K =  , 
+temperatura de ebulição da água em C = .", tempK, tempC)
+
+}
